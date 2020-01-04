63900660c2cb84054a48b0f2160e92810bb1ae2b
Peter Park <impeter9@gmail.com>
2020-01-04 12:52:34 -0800

    set up working proxy

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 /* eslint-disable no-console */
 const express = require('express');
-const path = require('path');
+const proxy = require('http-proxy-middleware');
 
+const options = {
+  target: 'http://localhost:3002', // target host
+  changeOrigin: true, // needed for virtual hosted sites
+  ws: true, // proxy websockets
+  pathRewrite: {
+    '^/api': '/api/estimates', // rewrite path
+  },
+};
+
+const apiProxy = proxy(options);
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '..', 'public')));
+app.use('/api', apiProxy);
 
 const port = 3000;
 app.listen(port, console.log(`listening on ${port}`));
+
+// http://localhost:3000/api/pricing/1
